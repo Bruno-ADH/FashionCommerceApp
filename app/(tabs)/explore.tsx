a9c82308a3dbc3471120df0fa0c6cf906e76e4b4
Bruno-ADH <brunoadenhouessou@gmail.com>
2025-11-02 15:36:31 +0100

    feat: prepare to 54sdk

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,14 +1,55 @@
-import { Text, View } from "react-native";
+import { useEffect } from "react";
+import { Button, Dimensions, GestureResponderEvent, Text, View } from "react-native";
+import Animated, { useAnimatedStyle, useSharedValue, withSpring } from 'react-native-reanimated';
+
+const {width: WIDTH, height: HEIGHT} = Dimensions.get("window")
 
 export default function Explore() {
+  const translate = useSharedValue(0)
+
+ const handlePress = ()=>{
+    translate.value += 5
+  }
+
+  useEffect(()=>{
+    console.log('translate.value :>> ', translate.value);
+  },[])
+
+  const animatedStyles = useAnimatedStyle(()=>{
+    console.log('translate.value :>> ', translate.value);
+    return({
+      top: withSpring(`${translate.value}%`),
+      left: withSpring(`${translate.value}%`)
+    })
+  })
+
+  function handleReload(event: GestureResponderEvent): void {
+    translate.value = 0
+  }
+
   return (
     <View
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        position: "relative",
+        gap: 10
       }}
     >
+      <Animated.View
+      style={[{
+        height: 100, 
+        backgroundColor: 'gold',
+        borderRadius: 20,
+        aspectRatio: 1/1,
+        position: "absolute",
+        top: 0,
+        left: 0
+      },animatedStyles]}
+    />
+    <Button title="Cliquez" onPress={handlePress}/>
+    <Button title="Reload" onPress={handleReload}/>
       <Text>Explore</Text>
     </View>
   );
